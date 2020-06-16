68468ad05fd6494c93c01f9c10e47a62891c9d91
Radosław Serba <rserba@gmail.com>
2020-06-16 22:27:16 +0200

    logo/author change + update to 6.8.4

--- storagecraft/spx/tools/chocolateyinstall.ps1
+++ storagecraft/spx/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://downloads.storagecraft.com/SP_Files/ShadowProtect_SPX-6.8.2-2_x86.msi'
-$url64      = 'https://downloads.storagecraft.com/SP_Files/ShadowProtect_SPX-6.8.2-2_x64.msi'
+$url        = 'https://downloads.storagecraft.com/SP_Files/ShadowProtect_SPX-6.8.4-5_x86.msi'
+$url64      = 'https://downloads.storagecraft.com/SP_Files/ShadowProtect_SPX-6.8.4-5_x64.msi'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -12,9 +12,9 @@ $packageArgs = @{
 
   softwareName  = 'spx*'
 
-  checksum      = '4E847BD62CE8550A586F7F633EEE70D18A389DF3F0E4CAE5941DD7F298AA403D'
+  checksum      = '7611178ACE269DDDB1645139A71DD384C6D44B3B838EDDA36380C9601AE990BB'
   checksumType  = 'sha256'
-  checksum64    = 'D871B74F63C6472C65F8A0A1A9304DBCE1003AF2E1E4332B230570B4559FA753'
+  checksum64    = '13ABE95378D2C4B43946F2B028FA32A2A22FDA38A803620A1E2B387CDBAE4648'
   checksumType64= 'sha256'
 
   silentArgs    = "/qn /norestart /lvoicewarmupx `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" IACCEPT=STORAGECRAFT.EULA"
